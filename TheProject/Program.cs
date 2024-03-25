cc7d9bfdace3f783cfc5b80fea77fb8e7120a29b
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-03-25 18:34:58 +0200

    Moved data folder and fixed namespaces

--- TheProject/Program.cs
+++ TheProject/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using TheProject.Data;
+using TheProject.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
