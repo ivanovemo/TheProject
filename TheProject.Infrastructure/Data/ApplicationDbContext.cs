2fb66d6932485ca3d2b6f3be6550ed0780532e81
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-03-28 20:00:50 +0200

    Configured DbContext file

--- TheProject.Infrastructure/Data/ApplicationDbContext.cs
+++ TheProject.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TheProject.Infrastructure.Data.Models;
 
 namespace TheProject.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -12,7 +13,33 @@ namespace TheProject.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<UserCourse>()
+                .HasKey(uc => new
+                {
+                    uc.UserId,
+                    uc.CourseId
+                });
+
+            builder.Entity<Course>()
+                .HasMany(c => c.Lectures)
+                .WithOne(l => l.Course)
+                .HasForeignKey(l => l.CourseId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Course>()
+                .HasMany(c => c.Reviews)
+                .WithOne(r => r.Course)
+                .HasForeignKey(r => r.CourseId)
+                .OnDelete(DeleteBehavior.Cascade);
+
             base.OnModelCreating(builder);
         }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Course> Courses { get; set; } = null!;
+        public DbSet<Lecture> Lectures { get; set; } = null!;
+        public DbSet<Instructor> Instructors { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+
     }
 }
