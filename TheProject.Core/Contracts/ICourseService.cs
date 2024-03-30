474659e44f9ed072f9e5646f68e6ff03dcbb5fee
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-03-30 03:11:30 +0200

    Added and implemented CourseService

---
+++ TheProject.Core/Contracts/ICourseService.cs
@@ -0,0 +1,16 @@
+﻿using TheProject.Core.Models.Course;
+using TheProject.Infrastructure.Data.Models;
+
+namespace TheProject.Core.Contracts
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseViewModel>> GetAllCoursesAsync();
+        Task<CourseViewModel> CourseDetailsAsync(Course currentCourse);
+        Task<bool> CourseExistsAsync(Guid courseId);
+        Task<Course?> GetCourseAsync(Guid courseId);
+        Task<IEnumerable<CourseViewModel>>GetUserCoursesAsync(string userId);
+        Task AddCourseToCollectionAsync(Guid courseId, string userId);
+        Task RemoveCourseFromCollectionAsync(Guid courseId, string userId);
+    }
+}
---
+++ TheProject.Core/Models/Course/CourseViewModel.cs
@@ -0,0 +1,49 @@
+﻿using static TheProject.Infrastructure.Constants.Constants;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using TheProject.Infrastructure.Data.Models;
+using ErrorMessages = TheProject.Infrastructure.Constants.Constants.ErrorMessages;
+
+namespace TheProject.Core.Models.Course
+{
+    public class CourseViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        [StringLength(CourseConstants.TitleMaxLength,
+            MinimumLength = CourseConstants.TitleMinLength,
+            ErrorMessage = ErrorMessages.Length)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        [StringLength(CourseConstants.DescriptionMaxLength,
+            MinimumLength = CourseConstants.DescriptionMinLength,
+            ErrorMessage = ErrorMessages.Length)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        public string Instructor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        public string InstructorPhoto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        public string StartDate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.Required)]
+        public string EndDate { get; set; } = string.Empty;
+
+        public int Interested { get; set; }
+
+        public string? Category { get; set; }
+
+        //public IList<Lecture> Lectures { get; set; } = new List<Lecture>();
+        //public IList<Review> Reviews { get; set; } = new List<Review>();
+        //public IList<UserCourse> UserCourses { get; set; } = new List<UserCourse>();
+    }
+}
