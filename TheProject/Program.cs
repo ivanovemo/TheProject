ec3b9838bca434456ac8bb432a1b6e4d27c12d1a
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-03-30 03:17:00 +0200

    Added scoped CourseService in Program.cs

--- TheProject/Program.cs
+++ TheProject/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TheProject.Core.Contracts;
+using TheProject.Core.Services;
 using TheProject.Infrastructure.Data;
+using TheProject.Infrastructure.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString)
+    .EnableSensitiveDataLogging());
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -22,6 +26,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options =>
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICourseService, CourseService>();
 
 var app = builder.Build();
 
@@ -43,9 +49,9 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapRazorPages();
 
 app.Run();
