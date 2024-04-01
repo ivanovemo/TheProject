9eb197eeb7b323cfd18c8fe8d63695951286010a
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-04-01 12:23:14 +0300

    Added CategoryViewModel

---
+++ TheProject.Core/Models/Course/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TheProject.Core.Models.Course
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
