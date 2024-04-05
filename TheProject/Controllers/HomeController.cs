5fc92b1c1d8f5982a02292d6fa8102c9d16c6f1c
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-04-05 16:12:09 +0300

    Added about us page

--- TheProject/Controllers/HomeController.cs
+++ TheProject/Controllers/HomeController.cs
@@ -16,6 +16,10 @@ namespace TheProject.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
