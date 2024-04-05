aad6ecf63ae36a6d4c5001015909874be57ad1b4
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-04-05 16:13:46 +0300

    Added contact us page

--- TheProject/Controllers/HomeController.cs
+++ TheProject/Controllers/HomeController.cs
@@ -20,6 +20,12 @@ namespace TheProject.Controllers
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
