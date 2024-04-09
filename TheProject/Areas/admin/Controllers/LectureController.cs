988afebe9d4e0edc5b21350c1afcf2e6dd74d8a2
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-04-09 08:46:14 +0300

    Admin can add lectures to a course + adding a partial view for adding a lecture form + working validations

---
+++ TheProject/Areas/admin/Controllers/LectureController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Globalization;
+using TheProject.Core.Contracts;
+using TheProject.Core.Models.Lecture;
+using DateFormat = TheProject.Infrastructure.Constants.Constants.Date;
+
+
+namespace TheProject.Areas.admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/[controller]/[Action]/{id?}")]
+    [Authorize(Roles = "admin")]
+    public class LectureController : Controller
+    {
+        private readonly ILectureService _lectureService;
+        public LectureController(ILectureService lectureService)
+        {
+            _lectureService = lectureService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new LectureViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(LectureViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    TempData["ErrorMessages"] = error.ErrorMessage;
+                }
+
+                var courseId = HttpContext.Request.Query["courseId"].ToString();
+
+                return RedirectToAction("Details", "Course", new { area = "", courseId });
+            }
+
+            DateTime startDate;
+
+            if (!DateTime.TryParseExact(model.StartDate, "yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out startDate))
+            {
+                ModelState.AddModelError(nameof(model.StartDate), $"Invalid format! The date should be in 'yyyy-MM-ddTHH:mm' format!");
+            }
+
+            try
+            {
+                await _lectureService.AddLectureAsync(model, startDate);
+                ModelState.Clear();
+                return RedirectToAction("Details", "Course", new { area = "", courseId = model.CourseId });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Date", ex.Message);
+                return View(model);
+            }
+        }
+    }
+}
