be9c6e5a5cf807af41db0595dd74c31a84226089
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-04-10 22:19:35 +0300

    Added more data for seeding the database

--- TheProject.Infrastructure/Data/Configuration/InstructorConfiguration.cs
+++ TheProject.Infrastructure/Data/Configuration/InstructorConfiguration.cs
@@ -59,6 +59,51 @@ namespace TheProject.Infrastructure.Data.Configuration
                     Bio = "Project management professional with extensive experience preparing candidates for the PMP® certification.",
                     Photo = "https://image.tmdb.org/t/p/original/A14lLCZYDhfYdBa0fFRpwMDiwRN.jpg",
                     CourseId = Guid.Parse("f5b840fe-cd8b-44e1-8c5e-5cdbec1d4a0f")
+                },
+                new Instructor
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Natalie",
+                    LastName = "Portman",
+                    Bio = "Natalie Portman, a renowned actress and passionate environmentalist, brings her wealth of knowledge and experience to the realm of sustainable living. With a strong belief in the power of renewable energy solutions, she has dedicated herself to advocating for a greener planet. Her courses are rich with practical insights on how to incorporate sustainable practices into daily life, drawing from her own journey towards living sustainably. Natalie's dedication to environmental activism is matched by her ability to inspire and engage her students. Through her comprehensive approach, she explores the various dimensions of sustainability, from green building techniques to sustainable agriculture. Her engaging teaching style combines theory with real-world applications, making her classes both informative and transformative. Natalie encourages her students to think critically about their environmental impact and empowers them to make meaningful changes. Her vision is to create a community of learners who are equipped to lead the way in sustainable living. With Natalie as your guide, you'll embark on a journey to understand the importance of sustainability and how you can be a part of the solution. Join her to discover the practices that will not only benefit the environment but also enhance your quality of life.",
+                    Photo = "https://facts.net/wp-content/uploads/2023/07/46-facts-about-natalie-portman-1690804650.jpg",
+                    CourseId = Guid.Parse("ae35af86-a051-46f0-9aef-980d4e7585ac")
+                },
+                new Instructor
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Anne",
+                    LastName = "Hathaway",
+                    Bio = "Anne Hathaway, with her deep passion for technology and education, spearheads our Introduction to Robotics course. Having spent years in the field of robotics engineering, Anne brings a wealth of practical knowledge and hands-on experience. Her course is designed to demystify the complex world of robotics and make it accessible to everyone, from beginners to more experienced enthusiasts. Anne's approach to teaching is deeply rooted in the belief that everyone has the potential to innovate. Through her engaging lectures and interactive workshops, students are encouraged to build and program their own robots, learning not just the technical skills but also the creativity and problem-solving abilities that are essential in robotics. Anne's dedication to her students goes beyond the classroom; she is committed to creating a supportive learning environment where questions are welcomed and exploration is encouraged. Her course covers a range of topics, from the basics of Arduino and Raspberry Pi to more advanced concepts in robotics engineering. Anne's mission is to inspire her students to pursue their interests in technology and to equip them with the skills needed to thrive in this exciting field. Join Anne Hathaway in this adventure into the world of robotics and discover the endless possibilities that await.",
+                    Photo = "https://www.usmagazine.com/wp-content/uploads/2023/11/Anne-Hathaway-revealed-that-she-was-told-that-her-career-would-be-over-by-the-time-she-turned-35-2.jpg?w=1000&quality=86&strip=all",
+                    CourseId = Guid.Parse("c65ee8c7-189a-4730-9171-d89324a534de")
+                },
+                new Instructor
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Penélope",
+                    LastName = "Cruz",
+                    Bio = "Penelope Cruz, an acclaimed actress with a rich background in language arts, has transitioned her passion into teaching Spanish to beginners. Her journey with languages began early in her career, allowing her to perform in a variety of roles across different cultures. Penelope's course, 'Spanish for Beginners: Master Basic Conversations', is a reflection of her love for the Spanish language and her desire to share it with others. Through her engaging teaching methods, Penelope emphasizes practical conversation skills, ensuring students gain confidence in their abilities to communicate. Her approach to teaching is infused with cultural insights, making learning a language not just about words but about understanding and appreciating a rich and vibrant culture. Penelope's classes are interactive and filled with real-life scenarios that prepare students for everyday conversations. With over a decade of experience in the film industry and numerous roles that required language mastery, Penelope brings authenticity and depth to her lessons. Her goal is to inspire a love for the Spanish language in her students, just as she has experienced in her travels and career. Join Penelope Cruz on this linguistic journey and unlock the door to new opportunities and connections through the beauty of the Spanish language.",
+                    Photo = "https://i.abcnewsfe.com/a/2e0c575c-cc96-480d-86a9-77f29924ac05/penelope-cruz-rt-gmh-240118_1705590269793_hpMain.jpg",
+                    CourseId = Guid.Parse("f7b36bbc-397a-411d-b15c-b51355eb4465")
+                },
+                new Instructor
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Meryl",
+                    LastName = "Streep",
+                    Bio = "Meryl Streep, an icon of the silver screen, brings her unparalleled experience and insight to the world of mental health first aid. With a career that spans decades and roles that have touched on a myriad of human experiences, Meryl is uniquely positioned to lead a course on supporting others and yourself in times of mental health crises. Her course, 'Mental Health First Aid: Supporting Others and Yourself', draws upon her deep understanding of the human psyche, explored through her diverse roles. Meryl's approach to teaching is compassionate and informed, aiming to destigmatize mental health issues and provide practical support techniques. She advocates for empathy, understanding, and direct action in assisting those facing mental health challenges. The course covers recognizing signs of mental health problems, offering initial help, and guiding someone towards appropriate care. Meryl's passion for mental health awareness is palpable in her lectures, making her an inspiring figure for those looking to make a difference. With her guidance, students learn not only how to help others but also the importance of self-care and resilience. Enroll in Meryl Streep's course to embark on a meaningful journey towards becoming a supportive presence in the lives of those around you.",
+                    Photo = "https://www.maconferenceforwomen.org/wp-content/uploads/sites/4/2017/04/meryl-streep.jpg",
+                    CourseId = Guid.Parse("78a56892-9dba-42cf-9328-e2cf026a5b97")
+                },
+                new Instructor
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Lupita",
+                    LastName = "Nyong'o",
+                    Bio = "Lupita Nyong'o, an Oscar-winning actress known for her profound and emotionally resonant performances, turns her talents to the art of storytelling through creative writing. With a history of roles that span diverse narratives and cultures, Lupita brings a unique perspective to her course, 'Creative Writing: Unleash Your Imagination'. She focuses on helping students unlock their creative potential, encouraging them to explore their own stories with depth, empathy, and originality. Lupita's teaching method is deeply interactive, involving students in exercises that challenge them to think critically and creatively. Her lessons delve into character development, plot structuring, and the nuances of language, all while fostering an environment of support and inspiration. Lupita believes that everyone has a story to tell and that the act of writing can be a powerful tool for self-expression and reflection. Her passion for storytelling is infectious, making her classes a vibrant space for aspiring writers. Whether you're looking to write your first short story or embark on a novel, Lupita Nyong'o's course offers the guidance, inspiration, and techniques to help you navigate the journey of creative writing.",
+                    Photo = "https://lh3.googleusercontent.com/vDTbynIYVfDFAT5NXckaRIKpMbO2N7bn_Gb57_1k26vPDbmjwjWgnx22AwRQpl6pCtHxOo1xiNOlUdBjascfRQEk9Oadz2aJSeZvFp2e",
+                    CourseId = Guid.Parse("6d38c7e6-84eb-411c-a528-53caecbdf5a5")
                 }
             };
 
--- TheProject.Infrastructure/Data/Configuration/LecturesConfiguration.cs
+++ TheProject.Infrastructure/Data/Configuration/LecturesConfiguration.cs
@@ -19,95 +19,278 @@ namespace TheProject.Infrastructure.Data.Configuration
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Introduction to HTML and CSS",
-                    Description = "Understanding the basics of web development with HTML and CSS.",
-                    StartDate = new DateTime(2024, 1, 15, 10, 0, 0),
+                    Title = "Setting Up Your Development Environment",
+                    Description = "Getting started with Node.js, Express, and MongoDB. Installation and environment setup for full stack development.",
+                    StartDate = new DateTime(2024, 1, 12, 10, 0, 0),
+                    Duration = 120,
+                    CourseId = Guid.Parse("9f9f3e8b-8fdd-4a38-b052-a5109123e2ab")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Building Your First React Application",
+                    Description = "Introduction to React and its core principles. Building a simple application to understand components, state, and props.",
+                    StartDate = new DateTime(2024, 1, 19, 10, 0, 0),
+                    Duration = 180,
+                    CourseId = Guid.Parse("9f9f3e8b-8fdd-4a38-b052-a5109123e2ab")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Connecting Frontend with Backend",
+                    Description = "Creating a seamless connection between React and Node.js. Understanding REST APIs and integrating MongoDB for data storage.",
+                    StartDate = new DateTime(2024, 1, 26, 10, 0, 0),
                     Duration = 150,
                     CourseId = Guid.Parse("9f9f3e8b-8fdd-4a38-b052-a5109123e2ab")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "JavaScript and the Web",
-                    Description = "Interactive web pages with JavaScript.",
-                    StartDate = new DateTime(2024, 1, 22, 10, 0, 0),
-                    Duration = 180,
-                    CourseId = Guid.Parse("9f9f3e8b-8fdd-4a38-b052-a5109123e2ab")
-                },
-
-                new Lecture
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "Python for Data Science",
-                    Description = "Introduction to Python and its libraries for data science.",
-                    StartDate = new DateTime(2024, 2, 20, 10, 0, 0),
+                    Title = "Python Programming for Data Science",
+                    Description = "An introduction to Python for data manipulation and analysis. Covering NumPy, pandas, and matplotlib libraries.",
+                    StartDate = new DateTime(2024, 2, 18, 10, 0, 0),
                     Duration = 90,
                     CourseId = Guid.Parse("6663d89a-3f07-4bea-b4f4-dc8b47efe599")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Exploratory Data Analysis",
-                    Description = "Analyzing data sets to summarize their main characteristics.",
-                    StartDate = new DateTime(2024, 2, 27, 10, 0, 0),
+                    Title = "Machine Learning Models with Scikit-learn",
+                    Description = "Exploring linear regression, logistic regression, and clustering techniques using the Scikit-learn library.",
+                    StartDate = new DateTime(2024, 2, 25, 10, 0, 0),
                     Duration = 120,
                     CourseId = Guid.Parse("6663d89a-3f07-4bea-b4f4-dc8b47efe599")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "SEO Fundamentals",
-                    Description = "Learn the basics of Search Engine Optimization to increase your website visibility.",
-                    StartDate = new DateTime(2024, 3, 25, 10, 0, 0),
-                    Duration = 75,
+                    Title = "Deep Learning Introduction with TensorFlow",
+                    Description = "An overview of neural networks and deep learning. Implementing a basic neural network using TensorFlow.",
+                    StartDate = new DateTime(2024, 3, 4, 10, 0, 0),
+                    Duration = 180,
+                    CourseId = Guid.Parse("6663d89a-3f07-4bea-b4f4-dc8b47efe599")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Understanding SEO",
+                    Description = "Diving deep into the world of search engine optimization, understanding keywords, and optimizing your website for Google.",
+                    StartDate = new DateTime(2024, 3, 23, 10, 0, 0),
+                    Duration = 100,
                     CourseId = Guid.Parse("412588b2-b00f-49dc-b261-04b65edfd9f3")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Social Media Marketing Strategies",
-                    Description = "Effective strategies for engaging with your audience on social media platforms.",
-                    StartDate = new DateTime(2024, 4, 1, 10, 0, 0),
-                    Duration = 60,
+                    Title = "Effective PPC Campaigns",
+                    Description = "Creating and managing pay-per-click campaigns on Google Ads and social media platforms.",
+                    StartDate = new DateTime(2024, 3, 30, 10, 0, 0),
+                    Duration = 120,
                     CourseId = Guid.Parse("412588b2-b00f-49dc-b261-04b65edfd9f3")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Design Principles: Composition and Layout",
-                    Description = "A deep dive into the core principles of graphic design, focusing on composition and layout.",
-                    StartDate = new DateTime(2024, 5, 1, 10, 0, 0),
+                    Title = "Social Media Marketing and Branding",
+                    Description = "Leveraging social media platforms to enhance brand visibility and engagement. Strategies for content creation and distribution.",
+                    StartDate = new DateTime(2024, 4, 6, 10, 0, 0),
+                    Duration = 90,
+                    CourseId = Guid.Parse("412588b2-b00f-49dc-b261-04b65edfd9f3")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Basics of Design: Color Theory and Typography",
+                    Description = "An exploration of color psychology, color palettes, and the importance of typography in design.",
+                    StartDate = new DateTime(2024, 4, 28, 10, 0, 0),
+                    Duration = 110,
+                    CourseId = Guid.Parse("557a4fcd-1f84-4487-a7bd-8240327c976b")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Adobe Photoshop Fundamentals",
+                    Description = "Mastering the basics of Adobe Photoshop for photo editing and graphic creation.",
+                    StartDate = new DateTime(2024, 5, 5, 10, 0, 0),
                     Duration = 180,
                     CourseId = Guid.Parse("557a4fcd-1f84-4487-a7bd-8240327c976b")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Typography in Design",
-                    Description = "Exploring the role of typography in graphic design and how to use it effectively.",
-                    StartDate = new DateTime(2024, 5, 8, 10, 0, 0),
-                    Duration = 300,
+                    Title = "Creating Effective Visuals for Web and Print",
+                    Description = "Designing impactful visuals for websites, social media, and printed materials.",
+                    StartDate = new DateTime(2024, 5, 12, 10, 0, 0),
+                    Duration = 140,
                     CourseId = Guid.Parse("557a4fcd-1f84-4487-a7bd-8240327c976b")
                 },
-                
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Project Management Basics",
-                    Description = "Covering the fundamental concepts and terminologies of project management.",
-                    StartDate = new DateTime(2024, 6, 5, 10, 0, 0),
+                    Title = "Introduction to Project Management Framework",
+                    Description = "Understanding the PMBOK framework and the process groups and knowledge areas of project management.",
+                    StartDate = new DateTime(2024, 6, 2, 10, 0, 0),
                     Duration = 120,
                     CourseId = Guid.Parse("f5b840fe-cd8b-44e1-8c5e-5cdbec1d4a0f")
                 },
                 new Lecture
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Preparing for the PMP Exam",
-                    Description = "Strategies and tips for tackling the PMP certification exam.",
-                    StartDate = new DateTime(2024, 6, 12, 10, 0, 0),
-                    Duration = 220,
+                    Title = "Risk Management and Mitigation",
+                    Description = "Identifying, analyzing, and responding to project risks to minimize their impact.",
+                    StartDate = new DateTime(2024, 6, 9, 10, 0, 0),
+                    Duration = 90,
                     CourseId = Guid.Parse("f5b840fe-cd8b-44e1-8c5e-5cdbec1d4a0f")
-                }
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Project Scheduling and Time Management",
+                    Description = "Techniques for creating realistic project schedules and ensuring timely project delivery.",
+                    StartDate = new DateTime(2024, 6, 16, 10, 0, 0),
+                    Duration = 150,
+                    CourseId = Guid.Parse("f5b840fe-cd8b-44e1-8c5e-5cdbec1d4a0f")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Renewable Energy Basics",
+                    Description = "An introduction to the different types of renewable energy sources and their impact on sustainable living.",
+                    StartDate = new DateTime(2024, 6, 20, 10, 0, 0),
+                    Duration = 120,
+                    CourseId = Guid.Parse("ae35af86-a051-46f0-9aef-980d4e7585ac")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Green Building Design",
+                    Description = "Exploring the principles of green building and sustainable design in modern architecture.",
+                    StartDate = new DateTime(2024, 6, 27, 10, 0, 0),
+                    Duration = 90,
+                    CourseId = Guid.Parse("ae35af86-a051-46f0-9aef-980d4e7585ac")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Sustainable Agriculture Techniques",
+                    Description = "Understanding sustainable farming practices and how they contribute to food security and environmental health.",
+                    StartDate = new DateTime(2024, 7, 4, 10, 0, 0),
+                    Duration = 105,
+                    CourseId = Guid.Parse("ae35af86-a051-46f0-9aef-980d4e7585ac")
+                },
+                
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Building Your First Robot",
+                    Description = "Step-by-step guidance on creating a simple robot, focusing on the basics of mechanical design and programming.",
+                    StartDate = new DateTime(2024, 7, 10, 10, 0, 0),
+                    Duration = 180,
+                    CourseId = Guid.Parse("c65ee8c7-189a-4730-9171-d89324a534de")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Arduino Programming Basics",
+                    Description = "Introduction to programming with Arduino, including sensor integration and basic robotics control.",
+                    StartDate = new DateTime(2024, 7, 17, 10, 0, 0),
+                    Duration = 90,
+                    CourseId = Guid.Parse("c65ee8c7-189a-4730-9171-d89324a534de")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Advanced Robotics with Raspberry Pi",
+                    Description = "Exploring more complex robotics projects using Raspberry Pi, focusing on IoT applications.",
+                    StartDate = new DateTime(2024, 7, 24, 10, 0, 0),
+                    Duration = 120,
+                    CourseId = Guid.Parse("c65ee8c7-189a-4730-9171-d89324a534de")
+                },
+                
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Basic Spanish Conversation",
+                    Description = "Learning common phrases and expressions to start speaking Spanish in daily situations.",
+                    StartDate = new DateTime(2024, 7, 30, 10, 0, 0),
+                    Duration = 90,
+                    CourseId = Guid.Parse("f7b36bbc-397a-411d-b15c-b51355eb4465")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Grammar Essentials",
+                    Description = "Covering the foundational grammar needed to build sentences and communicate effectively in Spanish.",
+                    StartDate = new DateTime(2024, 8, 6, 10, 0, 0),
+                    Duration = 75,
+                    CourseId = Guid.Parse("f7b36bbc-397a-411d-b15c-b51355eb4465")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Spanish Culture and Expressions",
+                    Description = "Understanding the cultural nuances and expressions that are unique to the Spanish language.",
+                    StartDate = new DateTime(2024, 8, 13, 10, 0, 0),
+                    Duration = 60,
+                    CourseId = Guid.Parse("f7b36bbc-397a-411d-b15c-b51355eb4465")
+                },
+                
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Understanding Mental Health",
+                    Description = "An overview of mental health, including common conditions and the importance of mental wellbeing.",
+                    StartDate = new DateTime(2024, 8, 20, 10, 0, 0),
+                    Duration = 90,
+                    CourseId = Guid.Parse("78a56892-9dba-42cf-9328-e2cf026a5b97")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Crisis Intervention Strategies",
+                    Description = "Techniques for providing immediate, short-term help to individuals experiencing a mental health crisis.",
+                    StartDate = new DateTime(2024, 8, 27, 10, 0, 0),
+                    Duration = 120,
+                    CourseId = Guid.Parse("78a56892-9dba-42cf-9328-e2cf026a5b97")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Promoting Mental Health Awareness",
+                    Description = "Strategies for raising awareness and reducing stigma associated with mental health issues.",
+                    StartDate = new DateTime(2024, 9, 3, 10, 0, 0),
+                    Duration = 75,
+                    CourseId = Guid.Parse("78a56892-9dba-42cf-9328-e2cf026a5b97")
+                },
+                
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Fundamentals of Creative Writing",
+                    Description = "Exploring the key elements of creative writing, including narrative structure, character development, and voice.",
+                    StartDate = new DateTime(2024, 9, 10, 10, 0, 0),
+                    Duration = 120,
+                    CourseId = Guid.Parse("6d38c7e6-84eb-411c-a528-53caecbdf5a5")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Crafting Compelling Characters",
+                    Description = "Techniques for creating complex, believable characters that engage readers.",
+                    StartDate = new DateTime(2024, 9, 17, 10, 0, 0),
+                    Duration = 90,
+                    CourseId = Guid.Parse("6d38c7e6-84eb-411c-a528-53caecbdf5a5")
+                },
+                new Lecture
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "The Art of Storytelling",
+                    Description = "Mastering the art of storytelling, focusing on plot development and the emotional journey of the characters.",
+                    StartDate = new DateTime(2024, 9, 24, 10, 0, 0),
+                    Duration = 105,
+                    CourseId = Guid.Parse("6d38c7e6-84eb-411c-a528-53caecbdf5a5")
+                },
+
             };
 
             return lectures;
